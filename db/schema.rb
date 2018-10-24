0ea59287cb3d11911f543ebf0fb12b50152e57d1
Marvio Rocha <marviorocha@marviorocha.com>
2018-10-24 03:21:49 -0300

    Add mobile menu and login sign - sigin out

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_22_135136) do
+ActiveRecord::Schema.define(version: 2018_10_23_164318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "calendars", force: :cascade do |t|
+    t.boolean "periodo"
+    t.date "day"
+    t.boolean "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
