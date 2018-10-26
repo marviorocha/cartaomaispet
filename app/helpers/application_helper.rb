12be8934d56cdb10fbd9e64631b5cf4ea89ad313
Marvio Rocha <marviorocha@marviorocha.com>
2018-10-26 02:10:33 -0300

    Add list pages and modules

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,11 @@
 module ApplicationHelper
 
 def title(page_title)
-  content_for(:title) { page_title}
+  content_for(:title) {page_title}
 end
 
+def company_name
+   "Cartão Mais Pet -"
+end
 
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -5,13 +5,13 @@ def saudacao
   time = Time.now
 
   if time > time.at_beginning_of_day
-    "Bom dia! " + current_user.name
+    "Bom dia! " + current_user.name.capitalize
   end
   if time > time.at_midday
-    "Boa tarde! " + current_user.name + current_user.name
+    "Boa tarde! " + current_user.name.capitalize
   end
-  if time < time.at_midday + 6.hour
-   "Boa Noite! " + current_user.name 
+  if time > time.at_midday + 6.hour
+   "Boa Noite! " + current_user.name.capitalize
   end
 end
 
