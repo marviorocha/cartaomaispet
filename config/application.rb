21ae808d9bd741ef94795f0c8298dba9a5bb93ba
Marvio Rocha <marviorocha@marviorocha.com>
2018-12-12 18:37:36 -0200

    relese

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,11 @@ module Cartaomaispet
     config.load_defaults 5.2
     config.assets.initialize_on_precompile = false
     config.i18n.default_locale = 'pt-BR'
+    config.cache_classes = true
+    config.serve_static_assets = true
+    config.assets.compile = true
+    config.assets.digest = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
