c8e8a2da44e54526cfeb0a66618b9e0a896373cb
Marvio Rocha <marviorocha@marviorocha.com>
2018-12-21 15:20:03 -0200

    Deploy first used capistrano

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,27 @@
 FROM ruby:2.5-slim
-RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs git
 
-RUN apt-get update
-RUN mkdir /cartaomaispet
-WORKDIR /cartaomaispet
-COPY Gemfile /cartaomaispet/Gemfile
-COPY Gemfile.lock /cartaomaispet/Gemfile.lock
-COPY . ./
-RUN bundle install
+RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs yarn vim openssh-server
 
+ENV HOME /var/www/myapp/
+
+RUN mkdir -p $HOME
+RUN mkdir -p /root/.ssh
+
+WORKDIR $HOME
+
+COPY Gemfile* ./
+COPY . .
+COPY id_rsa* /root/.ssh/
 ENV GEM_HOME="/usr/local/bundle"
 ENV PATH $GEM_HOME/bin:$GEM_HOME/gems/bin:$PATH
 
-COPY . /cartaomaispet
-RUN gem install bundler
-RUN bundle install --jobs 20 --retry 5
+RUN bundle install
+RUN bundle config --global frozen 1
+
+RUN apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+
+RUN apt-get update && apt-get upgrade -y
 EXPOSE 3000
-ENTRYPOINT ["bundle", "exec"]
-CMD ["bundle", "exec", "rails", "server", "-b", "0.0.0.0"]
+
+CMD bundle exec puma -C config/puma.rb
--- config/deploy.rb
+++ config/deploy.rb
@@ -8,42 +8,32 @@ set :repo_url, "https://github.com/marviorocha/cartaomaispet.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/var/www/cartaomaispet"
+ set :deploy_to, "/var/www/cartaomaispet"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
 
 # You can configure the Airbrussh format using :format_options.
 # These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+ set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
 
 # Default value for :pty is false
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/storage.yml", "config/master.key"
+ append :linked_files, "config/database.yml", "config/master.key"
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
-set :puma_access_log, "#{shared_path}/log/puma_access.log"
-set :puma_error_log, "#{shared_path}/log/puma_error.log"
-
-set :nginx_sites_available_path, "/etc/nginx/sites-available"
-set :nginx_sites_enabled_path, "/etc/nginx/sites-enabled"
-
-
 
 # Default value for local_user is ENV['USER']
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-set :keep_releases, 5
+ set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
