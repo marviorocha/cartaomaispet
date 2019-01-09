f593e58671379f2c75b17ee1d67b4439dc07f6fb
Marvio Rocha <marviorocha@marviorocha.com>
2019-01-09 21:16:05 -0200

    fix space

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ def title(page_title)
 end
 
 def company_name
-   "Cartão Mais Pet - "
+   "Cartão Mais Pet "
 end
 
 def select_menu
@@ -20,10 +20,11 @@ end
 def user_avatar
 
 if current_user.avatar
- image_tag(current_user.avatar, size:"150x150", class: "responsive circle align-center")
+image_tag(current_user.avatar, size:"150x150", class: "responsive circle align-center")
 else
 image_tag("https://picsum.photos/150", size:"150x150", class: "responsive circle align-center")
 end
+
 
 end
 
