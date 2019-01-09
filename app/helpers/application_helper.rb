dc05ec0a7c0c1857b5bdb18f3a0ea3c1984fdcd5
Marvio Rocha <marviorocha@marviorocha.com>
2019-01-09 20:23:10 -0200

    revert layout version

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,6 +17,15 @@ def select_menu
   end
 end
 
+def user_avatar
+
+if current_user.avatar
+ image_tag(current_user.avatar, size:"150x150", class: "responsive circle align-center")
+else
+image_tag("https://picsum.photos/150", size:"150x150", class: "responsive circle align-center")
+end
+
+end
 
 
 
