57191edb0747b015d2d8e6582953e0dae71fed36
Marvio Rocha <marviorocha@marviorocha.com>
2019-02-27 10:45:53 -0300

    fix deployment

--- config/application.rb
+++ config/application.rb
@@ -5,18 +5,11 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-ENV['RAILS_ADMIN_THEME'] = 'rollincode'
 
 module Cartaomaispet
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.assets.initialize_on_precompile = true
-    config.i18n.default_locale = 'pt-BR'
-    config.cache_classes = true
-    config.serve_static_assets = true
-    config.assets.compile = true
-    config.assets.digest = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
